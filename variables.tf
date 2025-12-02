d61fff8db19f51257e05b7b0987e3cd2fe764a7b
Patrice SIMO <simopatrice@yahoo.fr>
2025-12-02 12:08:07 +0100

    Mise a jour des variables

--- variables.tf
+++ variables.tf
@@ -48,5 +48,5 @@ variable "tags" {
 variable "github_owner" {
   description = "GitHub owner/organization for OIDC configuration"
   type        = string
-  default     = "simopatrice"  # Remplacez par votre nom d'utilisateur GitHub
+  default     = "Koumalo2023"
 }
